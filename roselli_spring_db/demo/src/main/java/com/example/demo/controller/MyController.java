f85b36433b7d9d933cf5be39abe243513eec3833
lai2-24 <lai2-24@itismeucci.com>
2024-01-31 10:40:22 +0100

    Added PUT, LIST

--- roselli_spring_db/demo/src/main/java/com/example/demo/controller/MyController.java
+++ roselli_spring_db/demo/src/main/java/com/example/demo/controller/MyController.java
@@ -1,11 +1,15 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,32 +25,51 @@ public class MyController {
     @Autowired
     PersonaRepository personaRepository;
 
-    @GetMapping("/")
-    public String hello(){
-        return "Hello world!";
-    }
 
-    @GetMapping("/list")
-    public String list(){
-        return personaRepository.findAll().toString();
-    }
 
+    //GET with request parameter
     @GetMapping("/{id}")
-    public Persona readOne(@PathVariable("id") long id){
+    public Persona readByRequest(@PathVariable long id){
         return personaRepository.getById(id);
     }
 
-    @PostMapping
+    @GetMapping("/list")
+    public List<Persona> list(){
+        return personaRepository.findAll();
+    } 
+
+    /*
+    @GetMapping("/find")
+    public Persona findByName(@RequestParam(value="name") String name){
+        return personaRepository.findByName(name);
+    }
+     */
+
+    @PostMapping()
     public Persona insert(@RequestBody Persona p){
         return personaRepository.save(p);
     }
 
-    @DeleteMapping("/delete")
+    @DeleteMapping()
     public ResponseEntity<String> delete(@RequestParam(value="id") long id){
         Persona p = personaRepository.getById(id);
         personaRepository.delete(p);
         return ResponseEntity.ok("Utente cancellato.");
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<String> update(@PathVariable long id, @RequestBody Persona personaUpdated){
+        Persona personaToUpdate = personaRepository.getById(id);
+
+        personaToUpdate.setNome(personaUpdated.getNome());
+        personaToUpdate.setCognome(personaUpdated.getCognome());
+        personaToUpdate.setEta(personaUpdated.getEta());
+
+        personaRepository.save(personaToUpdate);
+
+        return ResponseEntity.ok("Dati utente aggiornati.");
+    }
+    
     
     
 }
